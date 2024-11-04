06699307e4bb653e84d6f484072b0b90b7902bc5
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-11-05 02:43:01 +0330

    feat: put aws to new template p10

--- steampipe-plugin-aws/aws/table_aws_wellarchitected_lens_review_improvement.go
+++ steampipe-plugin-aws/aws/table_aws_wellarchitected_lens_review_improvement.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/opengovern/og-aws-describer-new/pkg/opengovernance-es-sdk"
+	opengovernance "github.com/opengovern/og-aws-describer-new/SDK/generated"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/wellarchitected"
