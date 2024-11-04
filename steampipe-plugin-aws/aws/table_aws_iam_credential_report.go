a20bf958edb2144d09262cb0dd7d0768d0b32c18
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-11-05 02:45:04 +0330

    feat: put aws to new template p20

--- steampipe-plugin-aws/aws/table_aws_iam_credential_report.go
+++ steampipe-plugin-aws/aws/table_aws_iam_credential_report.go
@@ -3,7 +3,7 @@ package aws
 import (
 	"context"
 
-	"github.com/opengovern/og-aws-describer-new/pkg/opengovernance-es-sdk"
+	opengovernance "github.com/opengovern/og-aws-describer-new/SDK/generated"
 	"github.com/turbot/go-kit/types"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
