3c89e27c9ed45be6d3b29002e5d8a7f4a19a3e60
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-11-05 02:43:10 +0330

    feat: put aws to new template p13

--- steampipe-plugin-aws/aws/table_aws_directory_service_certificate.go
+++ steampipe-plugin-aws/aws/table_aws_directory_service_certificate.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/opengovern/og-aws-describer-new/pkg/opengovernance-es-sdk"
+	opengovernance "github.com/opengovern/og-aws-describer-new/SDK/generated"
 
 	"github.com/aws/aws-sdk-go-v2/service/directoryservice/types"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
