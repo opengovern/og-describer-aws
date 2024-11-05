30729281e1b571546b49cdff245294cb50bb4f8b
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-11-05 03:54:19 +0330

    fix: imports

--- steampipe-plugin-aws/aws/table_aws_ec2_ami_shared.go
+++ steampipe-plugin-aws/aws/table_aws_ec2_ami_shared.go
@@ -3,7 +3,7 @@ package aws
 import (
 	"context"
 
-	"github.com/opengovern/og-describer-aws/pkg/opengovernance-es-sdk"
+	"github.com/opengovern/og-describer-aws/SDK/generated"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
