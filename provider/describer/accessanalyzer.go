a3ee37ef1aea161c785f672fecec12c2d429f132
artaasadi <arta-c@kaytu.io>
2024-11-10 02:20:56 +0100

    fix: fix wrappers

--- provider/describer/accessanalyzer.go
+++ provider/describer/accessanalyzer.go
@@ -2,6 +2,7 @@ package describer
 
 import (
 	"context"
+	"github.com/opengovern/og-describer-aws/pkg/sdk/models"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/accessanalyzer"
